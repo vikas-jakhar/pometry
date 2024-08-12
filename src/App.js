1b490e93edd9952511d3910580a2fe4d3471371c
vikas-jakhar <vikashjakhar2004@gmail.com>
2024-08-12 16:53:01 +0530

    common button update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
-import CommonHeading from './common/CommonHeading';
 
 function App() {
   return (
-    <div className="App">
-      <CommonHeading text="hlo" redText="hii" />
+    <div className="">
+      
     </div>
   );
 }
