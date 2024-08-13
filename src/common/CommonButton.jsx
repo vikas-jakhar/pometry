37f525e5ce243454a3900ce3f98111194416e322
vikas-jakhar <vikashjakhar2004@gmail.com>
2024-08-13 10:46:41 +0530

    change

--- src/common/CommonButton.jsx
+++ src/common/CommonButton.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const CommonButton = (props) => {
   return (
-    <button className={`${props.className} py-2.5 border duration-300 ease-linear hover:shadow-[0px_0px_0px_1px_white,0px_0px_0px_2px_black] rounded px-8 font-semibold text-base`}>{props.text}</button>
+    <button className={`${props.className} py-2.5 border duration-300 ease-linear relative rounded px-8 font-semibold text-base`}>{props.text}
+      <span className='absolute bg-black w-1/2 top-0 left-1/2 -translate-x-1/2 h-full'></span>
+    </button>
   )
 }
 
