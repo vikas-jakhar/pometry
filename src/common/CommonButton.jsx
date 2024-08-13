195406e09bc4db6bdc7537b98ed671a4d5e1e72c
vikas-jakhar <vikashjakhar2004@gmail.com>
2024-08-13 10:08:41 +0530

    header button update

--- src/common/CommonButton.jsx
+++ src/common/CommonButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CommonButton = (props) => {
   return (
-    <button className={`${props.className} py-2.5 border border-darkRed rounded px-8 font-semibold text-base`}>{props.text}</button>
+    <button className={`${props.className} py-2.5 border rounded px-8 font-semibold text-base`}>{props.text}</button>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import pometryLogo from "../assets/image/webp/pometry-logo.png";
+import { NAV_ITEMS_DATA } from '../common/Helper';
+import CommonButton from '../common/CommonButton';
 
 const Header = () => {
     return (
@@ -10,8 +12,11 @@ const Header = () => {
                         <img src={pometryLogo} alt="pometryLogo" className='w-full' />
                     </a>
                     <div className="flex flex-row items-center gap-8">
-                        <a href="/" className='font-normal text-base text-offBlack opacity-90'>Home</a>
+                        {NAV_ITEMS_DATA.map((i) => (
+                            <a key={i} href="/" className='font-normal text-base text-offBlack opacity-90 duration-300 ease-linear hover:bg-darkRed-gradient hover:text-transparent hover:bg-clip-text after:[] after:absolute after:bg-darkRed-gradient after:h-px after:w-0 after:right-0 after:bottom-0 after:rounded after:duration-300 after:ease-linear hover:after:w-full hover:after:right-auto hover:after:left-0 relative'>{i.text}</a>
+                        ))}
                     </div>
+                    <CommonButton text="Start Building" className="border-darkRed text-darkRed" />
                 </nav>
             </div>
         </div>
